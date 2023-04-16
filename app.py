6a125651dd0038ffcd44e3341214bd537230aefd
Aaron McGowan <47078754+HellfunDigital@users.noreply.github.com>
2023-04-17 05:06:36 +1000

    Update app.py

--- app.py
+++ app.py
@@ -23,56 +23,31 @@ def generate():
     prompt = f"Title: {data['title']}\nGenre: {data['genre']}\nCharacters: {data['characters']}\nPlot: {data['plot']}\n\n{data['prompt']}"
     print(f"Prompt: {prompt}")
 
-    num_agents = 2
-    prompts = [f"{prompt} [Part {i+1}/{num_agents}]" for i in range(num_agents)]
+    num_agents = 3
+    refinement_iterations = 2
+    refinement_agent_index = num_agents - 1
 
     openai.api_key = default_api_key
 
+    # Generate initial text
     generated_texts = []
-
-    for p in prompts:
+    for i in range(num_agents - 1):
+        p = f"{prompt} [Part {i + 1}/{num_agents - 1}]"
         response = openai.Completion.create(engine="text-davinci-002", prompt=p, max_tokens=150, n=1, stop=None, temperature=0.7)
-        print(f"API Response for {p}: {response}")
-        generated_text = response.choices[0].text.strip()
-        generated_texts.append(generated_text)
+        generated_texts.append(response.choices[0].text.strip())
 
-    combined_text = ""
-    for i, text in enumerate(generated_texts):
-        if i > 0:
-            text_part_1 = generated_texts[i - 1]
-            text_part_2 = text
+    # Iteratively refine the text
+    for _ in range(refinement_iterations):
+        for i in range(num_agents - 1):
+            text_part_1 = generated_texts[i]
+            text_part_2 = generated_texts[i + 1] if i + 1 < len(generated_texts) else ""
             gap_prompt = f"Continue the story smoothly between:\n\n{text_part_1}\n\nand\n\n{text_part_2}\n\nRefined transition:"
+            response = openai.Completion.create(engine="text-davinci-002", prompt=gap_prompt, max_tokens=30, n=1, stop=None, temperature=0.7)
+            refined_text = response.choices[0].text.strip()
+            generated_texts[i] = text_part_1 + " " + refined_text + " " + text_part_2
 
-            gap_response = openai.Completion.create(
-                engine="text-davinci-002",
-                prompt=gap_prompt,
-                max_tokens=500,
-                n=1,
-                stop=None,
-                temperature=0.5
-            )
-
-            refined_transition = gap_response.choices[0].text.strip()
-            combined_text += " " + refined_transition
-
-        combined_text += " " + text
-
+    combined_text = ' '.join(generated_texts)
     formatted_text = formatText(combined_text)
-    return jsonify(formatted_text)
-
-        # Add refinement process
-    refinement_prompt = f"Refine the following text for better consistency, coherence, and add gaps between different parts of the story:\n\n{combined_text}\n\nRefined Text:"
-    refined_response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=refinement_prompt,
-        max_tokens=1000,
-        n=1,
-        stop=None,
-        temperature=0.5
-    )
-    refined_text = refined_response.choices[0].text.strip()
-
-    formatted_text = formatText(refined_text)
     return jsonify(formatted_text)
 
 def formatText(text):
