74558093850a005a7122d7b3d3d2d1e798186438
Aaron McGowan <47078754+HellfunDigital@users.noreply.github.com>
2023-04-17 03:57:26 +1000

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def generate():
     prompt = f"Title: {data['title']}\nGenre: {data['genre']}\nCharacters: {data['characters']}\nPlot: {data['plot']}\n\n{data['prompt']}"
     print(f"Prompt: {prompt}")
 
-    num_agents = 5
+    num_agents = 3
     prompts = [f"{prompt} [Part {i+1}/{num_agents}]" for i in range(num_agents)]
 
     openai.api_key = default_api_key
